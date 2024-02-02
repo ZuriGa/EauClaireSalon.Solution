e2200f7fb242bb2262f073f2d214701e493119a7
ZuriGa <zurisadailopezmartinez@gmail.com>
2024-02-02 13:35:09 -0800

    add hairsalon context file

--- HairSalon/Models/HairSalonContext.cs
+++ HairSalon/Models/HairSalonContext.cs
@@ -4,6 +4,10 @@ namespace HairSalon.Models
 {
   public class HairSalonContext : DbContext
   {
-    // public DbSet<
+    public DbSet<Stylist> Stylists { get; set; }
+
+    public DbSet<Client> Clients { get; set; }
+
+    public HairSalonContext(DbContextOptions options) : base(options) { }
   }
 }
