e0c6181a5774ca3726fda637b60ba350ddb124c2
ZuriGa <zurisadailopezmartinez@gmail.com>
2024-02-02 15:33:30 -0800

    add stylist controller + home and index files

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -6,5 +6,42 @@ using System.Linq;
 
 namespace HairSalon.Controllers
 {
-  public class 
+  public class StylistController : Controller
+  {
+    private readonly HairSalonContext _db;
+
+    public StylistController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Stylist> model = _db.Stylist.ToList();
+      return View(model);
+    }
+
+    public ActionResult Creat()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Stylist stylist)
+    {
+      _db.Stylist.Add(stylist);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Stylist thisStylist = _db.Stylist
+                                  .Include(stylist => stylist.Clients)
+                                  .FirstOrDefault(stylist => stylist.ClientId == id);
+        return View(thisStylist);
+    }
+
+    
+  }
 }
